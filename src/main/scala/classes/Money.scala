27911e57a129b6a5ffb8acaae651bb986fea3d3c
Takeshi Emoto <takeshi.emoto@jointcrew.co.jp>
2020-06-10 09:33:37 +0900

    dev

--- src/main/scala/classes/Money.scala
+++ src/main/scala/classes/Money.scala
@@ -1,19 +1,22 @@
 package classes
 
 object Money {
+  def getInfo(): Unit = println("Money object")
+
   def apply(value: Int): Money = new Money(value)
 }
 
-class Money(val value: Int = 0) {
-  def minus(minus: Int): Unit = {
-    this.value - minus
+class Money(val value: Int) {
+  def minus(v: Int) = {
+    new Money(this.value - v)
   }
 }
 
 object Demo {
   def main(args: Array[String]): Unit = {
-    val base = new Money(100)
-    val discounted: Unit = base.minus(100)
+    println(Money.getInfo())
+    val base = Money(100)
+    val discounted = base.minus(50)
     println(discounted)
   }
 }
